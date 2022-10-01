6af8e513ff97d90aeb3f6ac1f9ab97010428a687
joao victor leite almeida <joaovictorfelicidade@outlook.com>
2022-10-01 11:20:05 -0300

    teste

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import requests
 from web import webscraping
 from help import helping
 from modo_velho import web
-from boot import boo
+from cambio2 import cambio
 
 #FUNÇAOES
 kk = web()
@@ -13,21 +13,21 @@ texto = webscraping()
     #Token
 TOKEN = ("key")
 bot = telebot.TeleBot(TOKEN)
-    #Commands
+    #Commands incialiaçao
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-     bot.reply_to(message, texto)
+     bot.reply_to(message,texto)
     #Help
 @bot.message_handler(commands=['help'])
 def send_welcome(message):
     bot.reply_to(message, helping)
-
+    #StartAntingo
 @bot.message_handler(commands=['start2'])
 def send_welcome(message):
     bot.reply_to(message, kk)
-
-@bot.message_handler(commands=['boot'])
+    #Cambio
+@bot.message_handler(commands=['cambio'])
 def send_welcome(message):
-     bot.reply_to(message,boo)
+     bot.reply_to(message,cambio)
     #Loop
 bot.polling()
