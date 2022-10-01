a49afee8859d0731e6200f5f5a1c72b8d35c4a18
joao victor leite almeida <joaovictorfelicidade@outlook.com>
2022-10-01 11:28:52 -0300

    teste 1

--- main.py
+++ main.py
@@ -1,22 +1,20 @@
 import telebot
-import requests
 from bs4 import BeautifulSoup
-import requests
 from web import webscraping
 from help import helping
 from modo_velho import web
 from cambio2 import cambio
 
 #FUNÇAOES
-kk = web()
-texto = webscraping()
+webtop = web()
+webnovo = webscraping()
     #Token
-TOKEN = ("key")
+TOKEN = ("1694856172:AAELynQUXMIMxKx2FdL4p7i9i0uJTbLalKI")
 bot = telebot.TeleBot(TOKEN)
     #Commands incialiaçao
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-     bot.reply_to(message,texto)
+     bot.reply_to(message,webnovo)
     #Help
 @bot.message_handler(commands=['help'])
 def send_welcome(message):
@@ -24,7 +22,7 @@ def send_welcome(message):
     #StartAntingo
 @bot.message_handler(commands=['start2'])
 def send_welcome(message):
-    bot.reply_to(message, kk)
+    bot.reply_to(message, webtop)
     #Cambio
 @bot.message_handler(commands=['cambio'])
 def send_welcome(message):
