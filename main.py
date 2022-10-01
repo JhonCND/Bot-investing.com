bf2f397cbe9033d359c41d935428c9c3e858d94b
joao victor leite almeida <joaovictorfelicidade@outlook.com>
2022-10-01 11:53:24 -0300

    updade

--- main.py
+++ main.py
@@ -1,22 +1,19 @@
 import telebot
-import requests
-from bs4 import BeautifulSoup
-import requests
 from web import webscraping
 from help import helping
 from modo_velho import web
-from boot import boo
+from cambio2 import cambio
 
 #FUNÇAOES
-kk = web()
-texto = webscraping()
+modovelho = web()
+wweb = webscraping()
     #Token
 TOKEN = ("key")
 bot = telebot.TeleBot('1694856172:AAELynQUXMIMxKx2FdL4p7i9i0uJTbLalKI')
     #Commands
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-     bot.reply_to(message, texto)
+     bot.reply_to(message, wweb)
     #Help
 @bot.message_handler(commands=['help'])
 def send_welcome(message):
@@ -24,10 +21,10 @@ def send_welcome(message):
 
 @bot.message_handler(commands=['start2'])
 def send_welcome(message):
-    bot.reply_to(message, kk)
+    bot.reply_to(message, modovelho)
 
-@bot.message_handler(commands=['boot'])
+@bot.message_handler(commands=['cambio'])
 def send_welcome(message):
-     bot.reply_to(message,boo)
+     bot.reply_to(message,cambio)
     #Loop
 bot.polling()
